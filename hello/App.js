02c39f372a5304d1065d5f06f9bc65e4218eae06
GustavoRamos14 <91494506+GustavoRamos14@users.noreply.github.com>
2022-09-30 09:40:34 -0300

    Add files via upload

---
+++ hello/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>{'\n'}</Text>
+        <Text>1. Hello React Native com Emulador {'\n'}</Text>
+
+        <Text>2. Hello React Native com Expo{'\n'}</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
